4b2bfffa4e12cd63876000352c698fac415f59bd
Victor Hugo <victor.hugospaixao@gmail.com>
2026-08-16 19:01:33 -0300

    Payment, one-to-one association

--- src/main/java/com/example/firstProject/entities/Order.java
+++ src/main/java/com/example/firstProject/entities/Order.java
@@ -33,6 +33,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
 
     public Order (){
 
@@ -61,6 +63,8 @@ public class Order implements Serializable {
         return OrderStatus.valueOf(orderStatus);
     }
 
+
+
     public void setOrderStatus(OrderStatus orderStatus) {
         if (orderStatus != null) {
             this.orderStatus = orderStatus.getCode();
@@ -77,6 +81,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems(){
