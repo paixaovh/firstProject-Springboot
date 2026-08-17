b72bacbb54a804a93225f8a6036749277c957165
Victor Hugo <victor.hugospaixao@gmail.com>
2026-08-17 13:28:02 -0300

    Exception Handling - update

--- src/main/java/com/example/firstProject/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/firstProject/resources/exceptions/ResourceExceptionHandler.java
@@ -22,7 +22,7 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(DatabaseException.class)
     public ResponseEntity<StandardError> dataBase(DatabaseException e, HttpServletRequest request){
-        String error = "DataBase Error: ";
+        String error = "Database Error";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
